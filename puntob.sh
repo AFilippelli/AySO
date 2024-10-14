a046e2908607c37779af7ee1b7fa305a79ff6894
AFilippelli <adrianfilippelli2@gmail.com>
2024-10-14 02:25:49 +0000

    subiendo vim de punto b y punto d

--- puntob.sh
+++ puntob.sh
@@ -1,1 +1,9 @@
-vagrant
+Acceder a particionar el disco:
+sudo fdisk/dev/sdc
+
+Formatear particiones:
+sudo mkfs.ext4 /dev/sdc
+
+Montar particiones:
+sudo mkdir /mnt/sdc1
+sudo mount /dev/sdc1 /mnt/sdc1
--- puntod.sh
+++ puntod.sh
@@ -1,1 +1,2 @@
-vagrant
+mkdir -p Estructura_Asmietrica/{{correo,clientes}/cartas{1..100},correo/carteros{1..10}
+
